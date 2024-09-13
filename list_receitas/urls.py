6cb54d2da7dec9119c38110901d046c9d60e23c6
Matheus Henrique <amhenrique157@gmail.com>
2024-09-13 09:25:15 -0300

    atualizações

--- list_receitas/urls.py
+++ list_receitas/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('receitas/', include('recipes.urls')),
+    path('', include('recipes.urls')),
   
 ]
