0632b4c9b3c18c681e96a714dcae2c94304094cb
Matheus Henrique <amhenrique157@gmail.com>
2024-10-02 09:51:10 -0300

    ALTERAÇÕES NO CORPO DO PROJETO E DIVISÃO DE SETORES

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # Create your views here.
 def home(request):
-    return render (request, 'recipes/home.html', context= {
+    return render (request, 'recipes/base_templates/base.html', context= {
         'name': 'Matheus'
     })
