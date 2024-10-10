8fd2428fc0426af3d9e8aa7254b9f5c4379f4189
Matheus Henrique <amhenrique157@gmail.com>
2024-10-10 09:18:00 -0300

    Inclusão da pagina  Detail da  receita

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ def home(request):
     return render (request, 'recipes/base_templates/base.html', context= {
         'name': 'Matheus'
     })
+
+def recipes(request, id):
+    return render (request, 'recipes/base_templates/recipe-view.html', context= {
+        'name': 'Matheus'
+    })
